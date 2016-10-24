084be8308b66da14d8fe819dc9445c9a96b6e764
dslwz2008 <dslwz2008@gmail.com>
2016-10-24 22:45:21 +0800

    add Resources

---
+++ Assets/Scripts/PlayerAnimatorManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Com.MyCompany.MyGame
+{
+    public class PlayerAnimatorManager : MonoBehaviour
+    {
+        #region MONOBEHAVIOUR MESSAGES
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        #endregion
+    }
+}
